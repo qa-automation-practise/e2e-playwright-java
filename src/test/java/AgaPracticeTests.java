f45da11be98c32958d69bca9c36f9b5fcfd16dbf
Agnieszka <agnieszka.antonczak@ttms.pl>
2022-10-21 00:01:45 +0200

    Resolved mvn command problem and used a code generator.

--- src/test/java/AgaPracticeTests.java
+++ src/test/java/AgaPracticeTests.java
@@ -6,8 +6,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 //TODO Try with chrome-beta
-//TODO Run the test with mvn command
+//TODO Run the test with mvn command - mvn exec:java -D"exec.mainClass"="AgaPracticeTests" (quotes on Windows!)
 
 public class AgaPracticeTests {
 
@@ -32,7 +34,7 @@ public class AgaPracticeTests {
 
     }
 
-    @Test
+    @Test(enabled = false)
     public void howToDoScreenshotsAndVideoRecords() {
         System.out.println(page.title());
         page.screenshot(new Page.ScreenshotOptions()
@@ -44,9 +46,38 @@ public class AgaPracticeTests {
                 .screenshot(new Locator.ScreenshotOptions().setPath(Paths.get("src/test/resources/Screens/Element_test.png")));
     }
 
-    @Test
+    @Test(enabled = false)
     public void howToUseSelectors() {
 
+    }
+
+    @Test(enabled = false)
+    public void howToGenerateCode(){
+        //PWDEBUG = 1
+        //mvn exec:java -e -D"exec.mainClass"="com.microsoft.playwright.CLI" -D"exec.args"="codegen https://www.saucedemo.com/"
+
+        // Go to https://www.saucedemo.com/
+        page.navigate("https://www.saucedemo.com/");
+        // Click [data-test="username"]
+        page.locator("[data-test=\"username\"]").click();
+        // Fill [data-test="username"]
+        page.locator("[data-test=\"username\"]").fill("test");
+        // Press Tab
+        page.locator("[data-test=\"username\"]").press("Tab");
+        // Fill [data-test="password"]
+        page.locator("[data-test=\"password\"]").fill("test");
+        // Double click [data-test="username"]
+        page.locator("[data-test=\"username\"]").dblclick();
+        // Fill [data-test="username"]
+        page.locator("[data-test=\"username\"]").fill("standard_user");
+        // Press Tab
+        page.locator("[data-test=\"username\"]").press("Tab");
+        // Fill [data-test="password"]
+        page.locator("[data-test=\"password\"]").fill("secret_sauce");
+        // Press Enter
+        page.locator("[data-test=\"password\"]").press("Enter");
+
+        assertThat(page).hasURL("https://www.saucedemo.com/inventory.html");
     }
 
     @AfterMethod
